d2903eb58b26ceec0ee82c543eeb7b993c665476
Jere <52434116+jereguerrero98@users.noreply.github.com>
2019-07-04 12:10:21 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+var sueldo; var resultado; 
+
+importe=document.getElementById('importe').value; 
+
+importe=parseInt (importe); 
+
+resultado= importe * 25 / 100 ; 
+
+document.getElementById ("resultado").value=resultado;
 }
