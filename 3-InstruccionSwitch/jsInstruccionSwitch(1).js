2b782cb9d7a428f44fc52fecfd5cb1c9c0fd77ae
Jere <52434116+jereguerrero98@users.noreply.github.com>
2019-07-10 12:22:08 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,33 @@
 function mostrar()
 {
 //tomo la edad  
+
+/* switch (variable) 
+case (POSIBLES VALORES)
+ej: case 'variable': ..... para cortar ejecucion usar termino break;
+ej: case 18 == "18" Verdadero
+ej: case 18 === "18" FALSO 
+
+*/
 var mesDelAño = document.getElementById('mes').value;
+
+switch (mesDelAño){
+    case "Enero":
+    alert ("que comiences bien el año!!!.");
+    break;
+
+    case "Marzo":
+    alert ("a clases!!!.");
+    break;
+
+    case "Julio":
+    alert ("se vienen las vacaciones!!!.");
+    break;
+
+    case "Diciembre":
+    alert ("Felices fiesta!!!.");
+    break; 
+}
 
 
 
