b3910bd837e0218218decd035ad965f75399f662
pepito <pepito@example.com>
2019-07-11 12:21:15 -0300

    modifique cuatro.js RECUPERATORIO

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
 
+    numeroUno=prompt ("Ingrese numero uno");
+    numeroDos=prompt ("Ingrese numero dos");
+
+    if (numeroUno==numeroDos){
+
+        alert (numeroUno+numeroDos);
+    
+    }else
+    { numeroUno=parseInt(numeroUno);
+      numeroDos=parseInt(numeroDos);
+
+    if (numeroUno>numeroDos){
+
+        alert (numeroUno/numeroDos);
+
+    }     
+    resultado=numeroUno/numeroDos
+    alert (resultado); 
 }
