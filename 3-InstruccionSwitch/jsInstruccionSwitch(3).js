c9c515917be7369a880dba43766129c6a4edd38f
Jere <52434116+jereguerrero98@users.noreply.github.com>
2019-07-18 23:47:53 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,20 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+//tomo la edad 
+    
+var mesDelAnio;
+
+mesDelAnio=document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+switch(mesDelAnio){
+    case "Febrero": 
+        alert("Este mes no tiene más de 29 días");
+    break;
+    default:
+        alert("Este mes tiene 30 días o más");
+    break;
+}
 	
 
 
